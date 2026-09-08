082b1a3622662f03fb5f585c0a62abea648c8b75
Axenos-dev <volk.aksen@gmail.com>
2026-09-08 00:33:13 -0700

    Simplify upload signing

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"crypto/sha256"
 	"fmt"
 	"log"
 	"os"
@@ -72,18 +71,12 @@ func main() {
 		}
 
 		// and initialize the service
-		var uploadKey []byte
-		if config.AdminToken != "" {
-			sum := sha256.Sum256([]byte("headlessgit-lfs-upload\x00" + config.AdminToken))
-			uploadKey = sum[:]
-		}
-
 		lfsService = lfs.NewService(
 			root.With(zap.String("service", "lfs")),
 			lfs.NewRegistry(db),
 			store,
 			config.LFS.PublicURL,
-			uploadKey,
+			[]byte(config.AdminToken),
 		)
 	}
 
